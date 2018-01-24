88aa7acb9824edcf1a15c56bff04ce5084a488e0
Kaleb Alsobrook <ka0961727@RK03-B1225-04.francistuttle.edu>
2018-01-24 08:31:57 -0600

    Version 4 (Added Chapter 7) (formatted)

--- src/Chapter7/C7_9.java
+++ src/Chapter7/C7_9.java
@@ -7,36 +7,31 @@ import java.util.*;
  * @author kalev Alsobrook
  */
 public class C7_9 {
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter list1: "); // 2 1 3
         int[] number1 = new int[scan.nextInt()];
-        for (int i = 0; i < number1.length; i++) 
-        {
+        for (int i = 0; i < number1.length; i++) {
             number1[i] = scan.nextInt();
         }
-        
+
         System.out.print("Enter list2: ");
         int[] number2 = new int[scan.nextInt()];
-        for (int j = 0; j < number2.length; j++) 
-        {
+        for (int j = 0; j < number2.length; j++) {
             number2[j] = scan.nextInt();
         }
 
-        if (equals(number1, number2) == true) 
-        { // you pass the array names
+        if (equals(number1, number2) == true) { // you pass the array names
             System.out.println("Arrays are identical!");
-        } 
-        else 
-        {
+        } else {
             System.out.println("Arrays are NOT identical!");
         }
 
     }//:)
 
-    public static boolean equals(int []list1, int []list2) //the method accepts 2 arrays
+    public static boolean equals(int[] list1, int[] list2) //the method accepts 2 arrays
     {
         return java.util.Arrays.equals(list1, list2); //this method will return true or false if the arrays are equal or not
     }
--- src/Chapter7/P7.java
+++ src/Chapter7/P7.java
@@ -3,18 +3,17 @@ package Chapter7;
 import java.util.*;
 
 /**
- *Asks for an array from user and finds average
- * 
+ * Asks for an array from user and finds average
+ *
  * @author kaleb alsobrook
  */
-public class P7 
-{
+public class P7 {
+
     static Scanner scan = new Scanner(System.in);
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         int average;
-        
+
         System.out.print("Insert Array: ");
         int numbers[] = new int[scan.nextInt()];
         P7.fillArray(numbers);
@@ -22,35 +21,29 @@ public class P7
         System.out.print("\n The Average is: " + average + "\n");
         P7.printArray(numbers);
     }
-    
+
     /**
      * Method takes user input and fills array
-     * 
+     *
      * @param list Array that is taken to be filled
      */
-    public static void fillArray(int list[])
-    {
-        for (int i = 0; i < list.length; i++)
-        {
+    public static void fillArray(int list[]) {
+        for (int i = 0; i < list.length; i++) {
             list[i] = scan.nextInt();
         }
     }
-    
-    public static int findAverage(int list[])
-    {
+
+    public static int findAverage(int list[]) {
         int total = 0;
-        for(int i = 0; i < list.length; i++)
-        {
+        for (int i = 0; i < list.length; i++) {
             total = total + list[i];
         }
         total = total / (list.length);
         return total;
     }
-    
-    public static void printArray(int list[])
-    {
-        for (int i = 0; i < list.length; i++)
-        {
+
+    public static void printArray(int list[]) {
+        for (int i = 0; i < list.length; i++) {
             System.out.print(list[i] + " ");
         }
     }
